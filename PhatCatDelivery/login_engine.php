766d671991c8c258e8470df30c18f4e9d5f9c0bf
clearspec11 <57431215+clearspec11@users.noreply.github.com>
2021-09-26 16:50:29 +1000

    added login,logout,register php docs

---
+++ PhatCatDelivery/login_engine.php
@@ -0,0 +1,35 @@
+<?php
+include('db_conn.php');
+include('session.php');
+
+if(isset($_POST)){
+$user_email = trim($_POST['login_email']);
+$user_password = trim($_POST['login_password']);
+    
+    $salt = "marathon";
+    $encrypted_password = crypt($user_password, $salt);
+
+
+$sql ="SELECT * FROM tb_customers WHERE email='$user_email'";
+$result = $mysqli->query($sql);
+//convert the result to array (the key of the array will be the column names of the table)
+$row=$result->fetch_array(MYSQLI_ASSOC);
+
+ if($row['email'] != $user_email ){
+   echo "We cannot find your account in our system.";
+ } else {
+   if($row['password']==$user_password) {
+
+     $_SESSION['session_id'] = $row['customer_id'];
+     $_SESSION['session_user']=$row['first_name'];
+     $_SESSION['session_email']=$row['email'];
+     #$_SESSION['session_access']=$row['access'];
+
+     echo "ok";
+   } else {
+     echo "Invalid password! Please try again.";
+   }
+ }
+}
+
+?>
---
+++ PhatCatDelivery/register_engine.php
@@ -0,0 +1,25 @@
+<?php
+
+include('db_conn.php'); //db connection
+
+if (isset($_POST['register'])){
+  $first_name=$_POST['fname'];
+  $last_name =$_POST['lname'];
+  $email = $_POST['email'];
+  $address =$_POST['address'];
+  $phone_number=$_POST['phone_number'];
+  $password = $_POST['password'];
+
+
+
+  $register = "INSERT INTO customers (`first_name`, `last_name`, `email`,  `address`,`phone_number`, `password`) VALUES ('$first_name','$last_name','$email','$address','$phone_number','$password')";
+
+
+  $mysqli->query($register);
+
+   echo "<script type='text/javascript'>alert('You are successfully registered!');
+  window.location='index.php';</script>";
+
+}
+
+ ?>
