8cc24f02ac3458ef46ea3d9f8e0fad46b5fbf970
clearspec11 <57431215+clearspec11@users.noreply.github.com>
2021-09-26 16:37:23 +1000

    database updated

--- assignment_2.sql
+++ assignment_2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 16, 2021 at 08:33 AM
+-- Generation Time: Sep 26, 2021 at 08:34 AM
 -- Server version: 10.4.20-MariaDB
 -- PHP Version: 8.0.8
 
@@ -24,102 +24,140 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `customers`
+-- Table structure for table `tb_cart`
 --
 
-CREATE TABLE `customers` (
-  `Customer_ID` int(11) NOT NULL,
-  `First_name` varchar(255) DEFAULT NULL,
-  `Last_name` varchar(255) NOT NULL,
-  `Email` varchar(255) DEFAULT NULL,
-  `Password` varchar(255) NOT NULL,
-  `Card_info` varchar(255) DEFAULT NULL,
-  `Age` int(11) NOT NULL,
-  `Mobile_number` int(11) NOT NULL,
-  `Address` varchar(255) NOT NULL,
-  `Account_balance` int(11) DEFAULT NULL
+CREATE TABLE `tb_cart` (
+  `cart_id` int(11) NOT NULL,
+  `customer_id` int(11) DEFAULT NULL,
+  `paid` varchar(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `customers`
---
-
-INSERT INTO `customers` (`Customer_ID`, `First_name`, `Last_name`, `Email`, `Password`, `Card_info`, `Age`, `Mobile_number`, `Address`, `Account_balance`) VALUES
-(1, 'leigh', 'beeton', 'leigh@gmail.com', '1234', '123456', 22, 412444575, '1 dally st mowbray', 55);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `menu`
+-- Table structure for table `tb_customers`
 --
 
-CREATE TABLE `menu` (
-  `Menu_No` int(11) DEFAULT NULL,
-  `item_Code` int(11) DEFAULT NULL,
+CREATE TABLE `tb_customers` (
+  `customer_id` int(11) NOT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `card_info` varchar(255) DEFAULT NULL,
+  `age` int(11) NOT NULL,
+  `mobile_number` int(11) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `account_balance` int(11) DEFAULT NULL,
+  `access` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_customers`
+--
+
+INSERT INTO `tb_customers` (`customer_id`, `first_name`, `last_name`, `email`, `password`, `card_info`, `age`, `mobile_number`, `address`, `account_balance`, `access`) VALUES
+(1, 'leigh', 'beeton', 'leigh', '1234', '123456', 22, 412444575, '1 dally st mowbray', 55, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_items`
+--
+
+CREATE TABLE `tb_items` (
+  `item_code` int(11) NOT NULL,
+  `item_price` float(11,2) NOT NULL,
+  `ingredients` varchar(255) DEFAULT NULL,
+  `item_name` varchar(255) NOT NULL,
+  `restaurant_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_items`
+--
+
+INSERT INTO `tb_items` (`item_code`, `item_price`, `ingredients`, `item_name`, `restaurant_id`) VALUES
+(1, 12.45, 'egg, bacon, bread', 'egg and bacon muffin', 1),
+(2, 69.69, 'cheese,tomato paste,flour,ham', 'pizza', 2),
+(3, 13.00, 'garlic,bread', 'garlic bread', 1),
+(5, 15.00, 'milk,flavouring', 'milkshake', 2),
+(6, 2.50, NULL, 'regular drink', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_menus`
+--
+
+CREATE TABLE `tb_menus` (
+  `menu_no` int(11) NOT NULL,
+  `item_code` int(11) DEFAULT NULL,
   `column_3` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `orders`
+-- Table structure for table `tb_orders`
 --
 
-CREATE TABLE `orders` (
-  `Customer_ID` int(11) NOT NULL,
-  `Order_No` int(11) NOT NULL,
-  `Price_total` int(11) NOT NULL,
-  `Collection_time` time DEFAULT NULL,
-  `Discount` int(11) DEFAULT NULL,
-  `items_List` varchar(255) NOT NULL
+CREATE TABLE `tb_orders` (
+  `customer_id` int(11) NOT NULL,
+  `order_no` int(11) NOT NULL,
+  `price_total` int(11) NOT NULL,
+  `collection_time` time DEFAULT NULL,
+  `discount` int(11) DEFAULT NULL,
+  `items_list` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `orders`
+-- Dumping data for table `tb_orders`
 --
 
-INSERT INTO `orders` (`Customer_ID`, `Order_No`, `Price_total`, `Collection_time`, `Discount`, `items_List`) VALUES
+INSERT INTO `tb_orders` (`customer_id`, `order_no`, `price_total`, `collection_time`, `discount`, `items_list`) VALUES
 (1, 1, 12, '12:00:00', NULL, 'f');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `restaurant`
+-- Table structure for table `tb_restaurant`
 --
 
-CREATE TABLE `restaurant` (
-  `Restaurant_ID` int(11) NOT NULL,
-  `Restaurant_Name` varchar(255) DEFAULT NULL,
-  `Address` varchar(255) DEFAULT NULL,
-  `Business_Code` int(11) DEFAULT NULL,
-  `Open_Hours` time DEFAULT NULL,
-  `Menu_NO` int(11) NOT NULL,
-  `Order_No` int(11) DEFAULT NULL
+CREATE TABLE `tb_restaurant` (
+  `restaurant_id` int(11) NOT NULL,
+  `restaurant_name` varchar(255) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `business_code` int(11) DEFAULT NULL,
+  `open_hours` time DEFAULT NULL,
+  `menu_no` int(11) NOT NULL,
+  `order_no` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `restaurant_manager`
+-- Table structure for table `tb_restaurant_managers`
 --
 
-CREATE TABLE `restaurant_manager` (
-  `Restraunt_Managers_ID` int(11) NOT NULL,
-  `First_Name` varchar(255) NOT NULL,
-  `Last_Name` varchar(255) NOT NULL,
-  `Password` varchar(255) NOT NULL,
-  `Restaurant_ID` int(11) DEFAULT NULL
+CREATE TABLE `tb_restaurant_managers` (
+  `restraunt_managers_id` int(11) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `restaurant_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `system_managers`
+-- Table structure for table `tb_system_managers`
 --
 
-CREATE TABLE `system_managers` (
-  `Manager_ID` int(11) NOT NULL,
-  `Password` int(11) NOT NULL
+CREATE TABLE `tb_system_managers` (
+  `manager_id` int(11) NOT NULL,
+  `password` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -127,87 +165,123 @@ CREATE TABLE `system_managers` (
 --
 
 --
--- Indexes for table `customers`
+-- Indexes for table `tb_cart`
 --
-ALTER TABLE `customers`
-  ADD PRIMARY KEY (`Customer_ID`);
+ALTER TABLE `tb_cart`
+  ADD PRIMARY KEY (`cart_id`);
 
 --
--- Indexes for table `orders`
+-- Indexes for table `tb_customers`
 --
-ALTER TABLE `orders`
-  ADD PRIMARY KEY (`Order_No`),
-  ADD KEY `Orders_customers_Customer_ID_fk` (`Customer_ID`);
+ALTER TABLE `tb_customers`
+  ADD PRIMARY KEY (`customer_id`);
 
 --
--- Indexes for table `restaurant`
+-- Indexes for table `tb_items`
 --
-ALTER TABLE `restaurant`
-  ADD PRIMARY KEY (`Restaurant_ID`),
-  ADD KEY `restaurant_orders_Order_No_fk` (`Order_No`);
+ALTER TABLE `tb_items`
+  ADD PRIMARY KEY (`item_code`);
 
 --
--- Indexes for table `restaurant_manager`
+-- Indexes for table `tb_menus`
 --
-ALTER TABLE `restaurant_manager`
-  ADD PRIMARY KEY (`Restraunt_Managers_ID`),
-  ADD KEY `Restaurant_Manager_restaurant_Restaurant_ID_fk` (`Restaurant_ID`);
+ALTER TABLE `tb_menus`
+  ADD KEY `menu_items_db_item_code_fk` (`item_code`);
 
 --
--- Indexes for table `system_managers`
+-- Indexes for table `tb_orders`
 --
-ALTER TABLE `system_managers`
-  ADD PRIMARY KEY (`Manager_ID`);
+ALTER TABLE `tb_orders`
+  ADD PRIMARY KEY (`order_no`),
+  ADD KEY `orders_customers_customer_id_fk` (`customer_id`);
+
+--
+-- Indexes for table `tb_restaurant`
+--
+ALTER TABLE `tb_restaurant`
+  ADD PRIMARY KEY (`restaurant_id`),
+  ADD KEY `restaurant_orders_order_no_fk` (`order_no`);
+
+--
+-- Indexes for table `tb_restaurant_managers`
+--
+ALTER TABLE `tb_restaurant_managers`
+  ADD PRIMARY KEY (`restraunt_managers_id`),
+  ADD KEY `restaurant_manager_restaurant_restaurant_id_fk` (`restaurant_id`);
+
+--
+-- Indexes for table `tb_system_managers`
+--
+ALTER TABLE `tb_system_managers`
+  ADD PRIMARY KEY (`manager_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `customers`
+-- AUTO_INCREMENT for table `tb_cart`
 --
-ALTER TABLE `customers`
-  MODIFY `Customer_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+ALTER TABLE `tb_cart`
+  MODIFY `cart_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `orders`
+-- AUTO_INCREMENT for table `tb_customers`
 --
-ALTER TABLE `orders`
-  MODIFY `Order_No` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+ALTER TABLE `tb_customers`
+  MODIFY `customer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT for table `restaurant`
+-- AUTO_INCREMENT for table `tb_items`
 --
-ALTER TABLE `restaurant`
-  MODIFY `Restaurant_ID` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `tb_items`
+  MODIFY `item_code` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
--- AUTO_INCREMENT for table `restaurant_manager`
+-- AUTO_INCREMENT for table `tb_orders`
 --
-ALTER TABLE `restaurant_manager`
-  MODIFY `Restraunt_Managers_ID` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `tb_orders`
+  MODIFY `order_no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tb_restaurant`
+--
+ALTER TABLE `tb_restaurant`
+  MODIFY `restaurant_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tb_restaurant_managers`
+--
+ALTER TABLE `tb_restaurant_managers`
+  MODIFY `restraunt_managers_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `orders`
+-- Constraints for table `tb_menus`
 --
-ALTER TABLE `orders`
-  ADD CONSTRAINT `Orders_customers_Customer_ID_fk` FOREIGN KEY (`Customer_ID`) REFERENCES `customers` (`Customer_ID`);
+ALTER TABLE `tb_menus`
+  ADD CONSTRAINT `tb_menus_tb_items_item_code_fk` FOREIGN KEY (`item_code`) REFERENCES `tb_items` (`item_code`);
 
 --
--- Constraints for table `restaurant`
+-- Constraints for table `tb_orders`
 --
-ALTER TABLE `restaurant`
-  ADD CONSTRAINT `restaurant_orders_Order_No_fk` FOREIGN KEY (`Order_No`) REFERENCES `orders` (`Order_No`);
+ALTER TABLE `tb_orders`
+  ADD CONSTRAINT `orders_customers_customer_id_fk` FOREIGN KEY (`customer_id`) REFERENCES `tb_customers` (`customer_id`);
 
 --
--- Constraints for table `restaurant_manager`
+-- Constraints for table `tb_restaurant`
 --
-ALTER TABLE `restaurant_manager`
-  ADD CONSTRAINT `Restaurant_Manager_restaurant_Restaurant_ID_fk` FOREIGN KEY (`Restaurant_ID`) REFERENCES `restaurant` (`Restaurant_ID`);
+ALTER TABLE `tb_restaurant`
+  ADD CONSTRAINT `restaurant_orders_order_no_fk` FOREIGN KEY (`order_no`) REFERENCES `tb_orders` (`order_no`);
+
+--
+-- Constraints for table `tb_restaurant_managers`
+--
+ALTER TABLE `tb_restaurant_managers`
+  ADD CONSTRAINT `restaurant_manager_restaurant_restaurant_id_fk` FOREIGN KEY (`restaurant_id`) REFERENCES `tb_restaurant` (`restaurant_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
