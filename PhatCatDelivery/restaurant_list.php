6f5593325edd3033f1ebda422691570643a58c31
Ted Krimmer <83988764+Tarnsky@users.noreply.github.com>
2021-10-01 17:40:12 +1000

    items restaurant

--- PhatCatDelivery/restaurant_list.php
+++ PhatCatDelivery/restaurant_list.php
@@ -2,11 +2,11 @@
  (only system manger will have access to this page) Not sure if we need this page or can just 
 use the editstaff page -->
 
-
-
 <?php
 include ('db_conn.php');
 include('cartFunctions.php');
+include ('session.php');
+
 if($session_id == ""){
      echo "<script type='text/javascript'>alert('You need to login!!');
     window.location='index.php';</script>";
@@ -122,7 +122,7 @@ if($session_id == ""){
           <th><?php echo $row['open_time'];?></th>
         </tr>
         <tr>
-          <th>close time</th>
+          <th>Close time</th>
           <th><?php echo $row['close_time'];?></th>
         </tr>
         <?php 
@@ -134,62 +134,58 @@ if($session_id == ""){
   <?php }} ?>
 
   </div>
- <?php
- include ('edituser.php');
- ?>
+  <li class="nav-item">
+            <a class="nav-link"  data-toggle="modal" data-target="#restaurantModal">Add restaurant</a>
+          </li>
 
-  <!-- Registration Modal Form -->
-  <div class="modal fade" id="regiModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <!-- Restaurant Modal Form -->
+
+  <div class="modal fade" id="restaurantModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title">Restaurant Registration</h5>
+          <h5 class="modal-title">Phatcat restaurant registration</h5>
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
           </button>
         </div>
         <div class="modal-body">
-          <form role = "form" class="regiForm" action = "register_engine.php" method = "post">
+          <form role = "form" class="regiForm" action = "restaurant_engine.php" method = "post">
             <table class = "responsive">
               <tr class="form-group">
-                <td style = "width: 40%"><label for="fname">First Name</label></td>
-                <td><input class = "form-control" type="text" id="fname" name ="fname" ></td>
+                <td style = "width: 40%"><label for="fname">Name</label></td>
+                <td><input class = "form-control" type="text" id="fname" name ="fname" required></td>
               </tr>
-
+              <tr>
+                <td>Address</td>
+                <td><input class = "form-control" type="text" id="address" name ="address" required></td>
+              </tr>
+              <tr>
+                <td>Business code</td>
+                <td><input class = "form-control" type="number" id="code" name ="code" required></td>
+              </tr>
+              <tr>
+                <td>Open Time</td>
+                <td><input class = "form-control" type="time" id="open" name ="open" required></td>
+              </tr>
+              <tr>
+                <td>Close Time</td>
+                <td><input class = "form-control" type="time" id="close" name ="close" required></td>
+              </tr>
               <tr class="form-group">
-                <td>Last Name</td>
-                <td><input  class = "form-control" type="text" id="lname" name = "lname" ></td>
-              </tr>
+          </td>
+        </tr>
+            </table>
+            <p><span id = "msg"></span></p>
 
-            <tr class="form-group">
-              <td>Email</td>
-              <td><input  class = "form-control" type="email" id="email" name ="email" ></td>
-            </tr>
-
-            <tr class="form-group">
-              <td>Password</td>
-              <td><input class = "form-control"  type="password" id="password" name = "password" pattern = "(?=.*\d)(?=.*[A-Za-z]).{6,8}" title = "Must contain at least one number, one letters and between 6 and 8 characters"  ></td>
-            </tr>
-
-            <tr class="form-group">
-              <td>Confirm Password</td>
-              <td><input class = "form-control"  type="password" id="confirm_password" name = "confirm_password"></td>
-            </tr>
-
-        </table>
-        <p><span id = "msg"></span></p>
-
-        <button class = "btn btn-danger float-right" type = "submit" id = "register" name = "register">Register</button>
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-      </form>
-
-
-    </div>
-    <div class="modal-footer">
+            <button class = "btn btn-danger float-right" type = "submit" id = "restaurantregister" name = "restaurantregister">Register restaurant</button>
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          </form>
+        </div>
+      </div>
     </div>
   </div>
-  </div>
-  </div>
+
 
 <!-- Footer -->
 
